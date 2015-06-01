46d7b7d80a2aa0b35b23a66c0eee6a12e44c25c0
chrischeuk <cc.cheuk@gmail.com>
2015-06-01 06:02:00 -0400

    first attempt

--- omniauth/config/routes.rb
+++ omniauth/config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root :to => 'welcome#index'
+#  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match '/auth/:provider/callback' => 'sessions#create', via: [:get, :post]
+
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
