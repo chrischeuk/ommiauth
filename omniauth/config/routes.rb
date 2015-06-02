d1923b25147d1ba7520f729f7da0c46f3a00aff6
chrischeuk <cc.cheuk@gmail.com>
2015-06-02 04:21:49 -0400

    successful login and logout

--- omniauth/config/routes.rb
+++ omniauth/config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session_get
+  end
+  # # devise_scope :user do
+  #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root :to => 'welcome#index'
 #  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-  match '/auth/:provider/callback' => 'sessions#create', via: [:get, :post]
-
-  match 'auth/failure', to: redirect('/'), via: [:get, :post]
-  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
--- omniauth/db/schema.rb
+++ omniauth/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150531123703) do
+ActiveRecord::Schema.define(version: 20150602040930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150531123703) do
     t.string   "uid"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
