1b86de21cc08a5fcf154b41d22c9a8145ac8644b
chrischeuk <cc.cheuk@gmail.com>
2015-06-01 22:54:20 -0400

    removed initializer

--- omniauth/app/models/user.rb
+++ omniauth/app/models/user.rb
@@ -2,5 +2,18 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
+       
+
+ def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
 end
--- omniauth/config/initializers/omniauth.rb
+++
@@ -1,6 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1434577556851684', '4521c816a9531d9fe183c2f9cea38671',
-  				 :scope => 'email,user_birthday,read_stream', :display => 'popup'
-end
